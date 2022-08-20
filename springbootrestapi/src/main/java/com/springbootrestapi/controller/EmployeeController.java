6b7a1a34b42d3ca68bff0a3ae356640f76caf06c
antonio <antonio@antonio-latitudee5570>
2022-08-20 08:27:05 +0100

    Added EmployeeController class

---
+++ springbootrestapi/src/main/java/com/springbootrestapi/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.springbootrestapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootrestapi.entity.Employee;
+import com.springbootrestapi.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository eRepo;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return eRepo.findAll();
+	}
+}
